99b85c0948c080617b867507f7af658256737ad6
fangtianqi <fangtianqi@southzn.com>
2022-10-24 17:45:14 +0800

    增加矩阵与标量乘法

--- src/math/matrix4x4.js
+++ src/math/matrix4x4.js
@@ -1,5 +1,9 @@
 // 矩阵
 class Matrix4x4 {
+    constructor(arr) {
+        this.val = arr;
+    }
+    // 单位矩阵
     static identity() {
         return [
             [1, 1, 1, 1],
@@ -8,4 +12,23 @@ class Matrix4x4 {
             [1, 1, 1, 1]
         ]
     }
+    // 与标量相乘
+    mulS(s) {
+       let result = []
+       this.val.forEach(element => {
+            let valArray = [];
+            element.forEach(val => {
+                valArray.push(val * s)
+            })
+            result.push(valArray)
+        });
+        return result;
+    }
+}
+
+// 矩阵测试代码
+export function testMatrix4x4() {
+    let test = new Matrix4x4([[1,2,2,3]]);
+    console.log(Matrix4x4.identity())
+    console.log(test.mulS(2))
 }
