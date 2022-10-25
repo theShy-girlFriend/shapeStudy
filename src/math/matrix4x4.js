d5195d95c58a86e5809a88b8bb3ed5dab78c36b1
fangtianqi <fangtianqi@southzn.com>
2022-10-25 13:42:48 +0800

    增加矩阵与矩阵乘法

--- src/math/matrix4x4.js
+++ src/math/matrix4x4.js
@@ -12,6 +12,20 @@ class Matrix4x4 {
             [1, 1, 1, 1]
         ]
     }
+    // 矩阵相乘
+    mul(Matrix4x4) {
+        let result = []
+        Matrix4x4.val.forEach((element,index) =>{
+            let valArray = []
+            let sum = 0;
+            element.forEach((val, valIndex) =>{
+                sum+= val * this.val[valIndex][index];
+            })
+            valArray.push(sum)
+            result.push(valArray)      
+        })
+        return result;
+    }
 
     // 矩阵与向量相乘
     mulV3(v3) {
@@ -49,4 +63,5 @@ export function testMatrix4x4() {
     console.log(Matrix4x4.identity())
     console.log(test.mulS(2))
     console.log(test.mulV3({x: 2, y: 3, z: 4}))
+    console.log(test.mul([[2], [5], [5], [3]]))
 }
